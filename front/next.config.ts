48be146c1faa2fd66742e63f76ce2a12acf6ba07
kimba <skyqwn@gmail.com>
2024-12-18 16:26:30 +0900

    react-query add

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devServer: {
+    host: "0.0.0.0", // 외부에서 접속할 수 있도록 설정
+  },
 };
 
 export default nextConfig;
