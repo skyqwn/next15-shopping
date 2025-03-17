852c67610f0f41ed392ae52700167a30da0cc1c9
kimba <skyqwn@gmail.com>
2025-03-17 21:42:14 +0900

    url test

--- front/src/api/httpMethod.ts
+++ front/src/api/httpMethod.ts
@@ -26,10 +26,7 @@ async function fetchWrapperWithTokenHandler<T>(
   uri: string,
   init?: RequestInit,
 ): Promise<ApiResponse<T>> {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}${uri}`,
-    init,
-  );
+  const response = await fetch(`http://server:4000/api${uri}`, init);
 
   try {
     const data = await response.json();
