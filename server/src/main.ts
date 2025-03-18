87811d90745d6a7548ede21b92a9f246f2690c1e
kimba <skyqwn@gmail.com>
2025-03-18 11:31:26 +0900

    edit

--- server/src/main.ts
+++ server/src/main.ts
@@ -15,20 +15,10 @@ async function bootstrap() {
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalFilters(new ZodExceptionFilter());
   app.useGlobalPipes(new ZodValidationPipe());
-  const environment = process.env.NODE_ENV;
-
-  let corsOrigin;
-
-  if (environment === 'production') {
-    corsOrigin = process.env.CLIENT_URL;
-    console.log(`Production environment: CORS origin set to ${corsOrigin}`);
-  } else {
-    corsOrigin = 'http://localhost:3000';
-    console.log(`Development environment: CORS origin set to ${corsOrigin}`);
-  }
 
   app.enableCors({
-    origin: corsOrigin,
+    origin: 'https://cicardi.store',
+    // origin: process.env.CLIENT_URL || 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
