66ef9be894cd0d5d8af391485063cee967091928
kimba <skyqwn@gmail.com>
2025-03-18 16:12:48 +0900

    ??

--- front/src/api/httpMethod.ts
+++ front/src/api/httpMethod.ts
@@ -30,7 +30,8 @@ async function fetchWrapperWithTokenHandler<T>(
   //   `${process.env.NEXT_PUBLIC_API_URL}${uri}`,
   //   init,
   // );
-  const response = await fetch(`https://www.cicardi.store${uri}`, init);
+  const response = await fetch(`http://sever:4000/api${uri}`, init);
+  // const response = await fetch(`http://13.125.84.188/api${uri}`, init);
 
   try {
     if (!response.ok)
--- front/src/constants/api.ts
+++ front/src/constants/api.ts
@@ -1,5 +1,3 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-
 export const END_POINTS = {
   USER_PROFILE: `/auth/me`,
   AUTH_STATUS: `/auth/status`,
