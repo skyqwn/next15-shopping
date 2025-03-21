a2040dcd1cd82074e0cc5985f3baa8502d13face
kimba <skyqwn@gmail.com>
2025-03-21 10:27:29 +0900

    docker build 변경11

--- front/src/api/httpMethod.ts
+++ front/src/api/httpMethod.ts
@@ -1,6 +1,6 @@
 export function createInit<Body extends object | FormData>(
   body?: Body,
-  cache: RequestCache = "no-store",
+  cache: RequestCache = "default",
 ): RequestInit {
   const headers: Record<string, string> = {};
 
@@ -26,11 +26,29 @@ async function fetchWrapperWithTokenHandler<T>(
   uri: string,
   init?: RequestInit,
 ): Promise<ApiResponse<T>> {
-  const apiUrl =
-    process.env.NEXT_PUBLIC_API_URL || "https://www.cicardi.store/api";
-  const response = await fetch(`${apiUrl}${uri}`, init);
+  // const apiUrl =
+  //   process.env.NEXT_PUBLIC_API_URL || "https://www.cicardi.store/api";
+  // const response = await fetch(`${apiUrl}${uri}`, init);
+
+  const baseUrl =
+    typeof window === "undefined"
+      ? (
+          process.env.NEXT_PUBLIC_API_URL || "https://www.cicardi.store/api"
+        ).replace("https://www.cicardi.store/api", "http://server:4000/api")
+      : process.env.NEXT_PUBLIC_API_URL || "https://www.cicardi.store/api";
+
+  if (!baseUrl) {
+    console.error("Base URL is not defined:", {
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    });
+    throw new Error("Base URL (NEXT_PUBLIC_API_URL) is not defined");
+  }
+
+  const fullUrl = `${baseUrl}${uri.startsWith("/") ? "" : "/"}${uri}`;
+  console.log("Fetching URL:", fullUrl);
 
   try {
+    const response = await fetch(fullUrl, init);
     if (!response.ok)
       throw new Error(`Failed to fetch ${uri}: ${response.statusText}`);
     const data = await response.json();
